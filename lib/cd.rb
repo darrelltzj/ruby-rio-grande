254515acee5899b36c9d2f78a69899c66af7b10a
Darrell Teo <darrelltzj@gmail.com>
2017-04-26 20:58:29 +0800

    cd done

--- lib/cd.rb
+++ lib/cd.rb
@@ -1,4 +1,11 @@
 require_relative 'item.rb'
 
 class Cd < Item
+  attr_accessor :tracks, :artist, :run_time
+  def initialize(name, price, tracks, artist, run_time)
+    @tracks = tracks
+    @artist = artist
+    @run_time = run_time
+    super(name, price, quantity, description)
+  end
 end
--- spec/cd_spec.rb
+++ spec/cd_spec.rb
@@ -4,10 +4,70 @@ require_relative '../lib/cd'
 describe Cd do
   before(:context) do
     # initialize item
+    @cd  = Cd.new('Test Name', 15.00, 7, 'Test Artist', 60)
   end
 
   # check initialization
   # check that it is an extended from Item
   # check that it is an instance of Cd
+  describe 'Initialization' do
+    it 'should be a cd' do
+      expect(@cd).to be_instance_of(Cd)
+    end
+
+    it 'should be an item' do
+      expect(@cd).to be_kind_of(Item)
+    end
+
+    it 'is assigned a name' do
+      expect(@cd.name).to eq('Test Name')
+    end
+
+    it 'is assigned a price' do
+      expect(@cd.price).to eq(15.00)
+    end
+
+    it 'is assigned tracks' do
+      expect(@cd.tracks).to eq(7)
+    end
+
+    it 'is assigned an artist' do
+      expect(@cd.artist).to eq('Test Artist')
+    end
+
+    it 'is assigned an run_time' do
+      expect(@cd.run_time).to eq(60)
+    end
+  end
+
   # check getters and setters
+  describe 'getters and setters' do
+    it 'assigns and reads the name' do
+      @cd.name = 'Another Name'
+      expect(@cd.name).to eq('Another Name')
+    end
+
+    it 'assigns and reads the price' do
+      @cd.price = 20.00
+      expect(@cd.price).to eq(20.00)
+    end
+
+    it 'assigns and reads the tracks' do
+      @cd.tracks = 5
+      expect(@cd.tracks).to eq(5)
+    end
+
+    it 'assigns and reads the artist' do
+      @cd.artist = 'Another Artist'
+      expect(@cd.artist).to eq('Another Artist')
+    end
+
+    it 'assigns and reads the run_time' do
+      @cd.run_time = 40
+      expect(@cd.run_time).to eq(40)
+    end
+  end
 end
+# * Tracks (number of)
+# * Artist
+# * Run time
